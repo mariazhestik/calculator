468e5787b6951cf3e1f31e1482c23aa0a4bfa7d9
mariazhestik <mariazhestik@gmail.com>
2025-04-02 17:58:39 +0100

    final logic

--- javascript.js
+++ javascript.js
@@ -43,12 +43,15 @@ const button6 = document.querySelector(".btn6");
 const button7 = document.querySelector(".btn7");
 const button8 = document.querySelector(".btn8");
 const button9 = document.querySelector(".btn9");
+const button0 = document.querySelector(".btn0");
 
 const equalOp = document.querySelector(".equal");
 const addOp = document.querySelector(".add");
 const substractOp = document.querySelector(".substract");
 const multiplyOp = document.querySelector(".multiply");
 const divideOp = document.querySelector(".divide");
+
+const cancelOp = document.querySelector(".cancel");
 
 button1.addEventListener('click', () => inputNumber(button1.textContent));
 button2.addEventListener('click', () => inputNumber(button2.textContent));
@@ -59,16 +62,26 @@ button6.addEventListener('click', () => inputNumber(button6.textContent));
 button7.addEventListener('click', () => inputNumber(button7.textContent));
 button8.addEventListener('click', () => inputNumber(button8.textContent));
 button9.addEventListener('click', () => inputNumber(button9.textContent));
+button0.addEventListener('click', () => inputNumber(button0.textContent));
+cancelOp.addEventListener('click', clearCalculator);
 
 equalOp.addEventListener('click', calculateResult);
-addOp.addEventListener('click', () => getOperator(equalOp.textContent));
-substractOp.addEventListener('click', () => getOperator(equalOp.textContent));
-multiplyOp.addEventListener('click', () => getOperator(equalOp.textContent));
-divideOp.addEventListener('click', () => getOperator(equalOp.textContent));
+addOp.addEventListener('click', () => getOperator('+'));
+substractOp.addEventListener('click', () => getOperator('-'));
+multiplyOp.addEventListener('click', () => getOperator('*'));
+divideOp.addEventListener('click', () => getOperator('/'));
 
 function updateDisplay(value) {
     const display = document.querySelector(".current-screen");
     display.textContent = value;
+}
+
+function clearCalculator() {
+    number1 = 0;
+    number2 = 0;
+    operator = '';
+    currentInput = '';
+    updateDisplay('0');
 }
 
 function inputNumber(number) {
@@ -85,7 +98,7 @@ function getOperator(selectedOperator) {
 }
 
 function calculateResult() {
-    if (currentInput === '' || selectedOperator === '') return;
+    if (currentInput === '' || operator === '') return;
 
     number2 = parseFloat(currentInput);
     const result = operate(operator, number1, number2);
