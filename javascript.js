b56785affb6133e8d5f933b526cb74a420aa387b
mariazhestik <mariazhestik@gmail.com>
2025-04-02 17:12:54 +0100

    add event listeners

--- javascript.js
+++ javascript.js
@@ -42,8 +42,29 @@ const button6 = document.querySelector(".btn6");
 const button7 = document.querySelector(".btn7");
 const button8 = document.querySelector(".btn8");
 const button9 = document.querySelector(".btn9");
+
 const equalOp = document.querySelector(".equal");
 const addOp = document.querySelector(".add");
 const substractOp = document.querySelector(".substract");
 const multiplyOp = document.querySelector(".multiply");
-const divideOP = document.querySelector(".divide");
+const divideOp = document.querySelector(".divide");
+
+button1.addEventListener('click', inputNumber(button1.textContent));
+button2.addEventListener('click', inputNumber(button2.textContent));
+button3.addEventListener('click', inputNumber(button3.textContent));
+button4.addEventListener('click', inputNumber(button4.textContent));
+button5.addEventListener('click', inputNumber(button5.textContent));
+button6.addEventListener('click', inputNumber(button6.textContent));
+button7.addEventListener('click', inputNumber(button7.textContent));
+button8.addEventListener('click', inputNumber(button8.textContent));
+button9.addEventListener('click', inputNumber(button9.textContent));
+
+equalOp.addEventListener('click', () => getOperator(equalOp.textContent));
+addOp.addEventListener('click', () => getOperator(equalOp.textContent));
+substractOp.addEventListener('click', () => getOperator(equalOp.textContent));
+multiplyOp.addEventListener('click', () => getOperator(equalOp.textContent));
+divideOp.addEventListener('click', () => getOperator(equalOp.textContent));
+
+function inputNumber(number) {
+
+}
